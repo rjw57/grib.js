9806095716ffe7768ead12fda32348a12670ef84
Rich Wareham <rjw57@cam.ac.uk>
2013-11-26 17:17:41 +0000

    first test suite

---
+++ test/readBuffer.js
@@ -0,0 +1,36 @@
+var assert = require('assert');
+var fs = require('fs');
+
+var grib = require('../grib');
+
+var tpcprblty_grib2 = null;
+
+// Load some sample data into a buffer
+beforeEach(function(done) {
+  fs.readFile('samples/tpcprblty.grib2', function(err, data) {
+    tpcprblty_grib2 = data;
+    done(err);
+  });
+});
+
+describe('grib', function() {
+  describe('#readBuffer()', function() {
+    it('should return null when no buffer given', function() {
+      assert.strictEqual(null, grib.readBuffer());
+    });
+  });
+
+  describe('#readBuffer()', function() {
+    it('should return an array of messages with a valid buffer', function() {
+      var msgs = grib.readBuffer(tpcprblty_grib2);
+      assert.ok(msgs.length >= 1);
+      assert.ok(msgs[0]);
+    });
+  });
+
+  describe('#readBuffer()', function() {
+    it('should throw an error with an invalid buffer', function() {
+      assert.throws(function() { grib.readBuffer("one, two, three"); });
+    });
+  });
+});
