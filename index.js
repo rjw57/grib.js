5cce4f4b0ed8544040cb6a6d2e9b4697eb6f02a3
Rich Wareham <rjw57@cam.ac.uk>
2013-11-28 11:35:56 +0000

    fix flux.grb parsing (allow junk at end of file)

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var Dissolve = require('dissolve');
+var gribParse = require('./lib/parser');
+
+var tables = require('./lib/tables');
+for(var tableName in tables.tables) {
+  exports[tableName] = tables.tables[tableName];
+}
 
 // NODE.js specific
 exports.readFile = function(fileName, cb) {
@@ -10,7 +15,7 @@ exports.readFile = function(fileName, cb) {
 }
 
 exports.readBuffer = function(buffer, cb) {
-  var parser = createParser_(), msg, msgs = [];
+  var parser = gribParse.createParser(), msg, msgs = [];
 
   // Write the contents of the buffer catching any parse errors
   try {
@@ -21,44 +26,10 @@ exports.readBuffer = function(buffer, cb) {
 
   // Read all the parsed messages
   while(msg = parser.read()) { msgs.push(msg); }
+
+  // If no messages were parsed throw an error
+  if(msgs.length == 0) { return cb(new Error('No GRIB messages could be decoded')); }
+
   cb(null, msgs);
 }
 
-var createParser_ = function() {
-  return Dissolve().loop('messages', function() { this
-    // GRIB indicator (section 0)
-    .string('magic', 4)
-    .tap('indicator', function() {
-      if(this.vars.magic != "GRIB") throw new Error("Invalid magic number. Expected 'GRIB'.");
-
-      this
-        .buffer('reserved', 2)
-        .uint8('discipline')
-        .uint8('edition')
-        .uint64be('byteLength');
-    })
-
-    // Individual sections
-    .loop('sections', function(end) { this
-      .uint32be('sectionByteLength')
-      .tap(function() {
-        // If the section length is 0x37373737, this is actually "7777" and is thus the
-        // end section
-        if(this.vars.sectionByteLength == 0x37373737) {
-          return end(true);
-        }
-
-        // Otherwise, parse remainder of section
-        this
-          .uint8('sectionNumber')
-          .tap(function() { this.buffer('sectionContents', this.vars.sectionByteLength-5); })
-      })
-    })
-
-    // Finish off by pushing message and clearing state
-    .tap(function() {
-      this.push(this.vars);
-      this.vars = {};
-    });
-  });
-}
--- test/basicParsing.js
+++ test/basicParsing.js
@@ -50,4 +50,37 @@ describe('tpcprblty.grib2', function() {
   it('should have 69 records', function() {
     assert.strictEqual(msgs.length, 69);
   });
+
+  it('should have Analysis records for 2009-11-09 12:00:00', function() {
+    for(var m_idx in msgs) {
+      var msg = msgs[m_idx];
+      assert.strictEqual(msg.referenceTime.value, new Date(2009, 10, 09, 12, 00, 00).value);
+      assert.strictEqual(msg.referenceTimeSignificance, grib.ReferenceTimeSignificance.ANALYSIS);
+    }
+  });
+});
+
+describe('flux.grb', function() {
+  var msgs;
+
+  // Read in file before each test (to guard against modification)
+  beforeEach(function(done) {
+    grib.readFile('samples/flux.grb', function(err, msgs_) {
+      if(err) return done(err);
+      msgs = msgs_;
+      done();
+    });
+  });
+
+  it('should have 4 records', function() {
+    assert.strictEqual(msgs.length, 4);
+  });
+
+  it('should have Start of Forecast records for 2004-02-29 12:00:00', function() {
+    for(var m_idx in msgs) {
+      var msg = msgs[m_idx];
+      assert.strictEqual(msg.referenceTime.value, new Date(2004, 02, 29, 12, 00, 00).value);
+      assert.strictEqual(msg.referenceTimeSignificance, grib.ReferenceTimeSignificance.START_OF_FORECAST);
+    }
+  });
 });
