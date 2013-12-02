625cd2f3f4c4f5432d91469ec08725136868177d
Rich Wareham <rjw57@cam.ac.uk>
2013-12-02 14:51:51 +0000

    remove old code from index.js

--- index.js
+++ index.js
@@ -29,29 +29,3 @@ exports.readUrl = function(url, cb) {
     exports.readData(data, cb);
   });
 }
-
-// NODE.js specific
-exports.readFile = function(fileName, cb) {
-  var fs = require('fs');
-  fs.readFile(fileName, function(err, data) {
-    if(err) { cb(err); return; }
-    exports.readBuffer(data, cb);
-  });
-}
-
-exports.readBuffer = function(buffer, cb) {
-  var msgs;
-
-  // Write the contents of the buffer catching any parse errors
-  try {
-    msgs = gribParse.parseBuffer(buffer);
-  } catch(e) {
-    return cb(e, null);
-  }
-
-  // If no messages were parsed throw an error
-  if(msgs.length == 0) { return cb(new Error('No GRIB messages could be decoded')); }
-
-  cb(null, msgs);
-}
-
